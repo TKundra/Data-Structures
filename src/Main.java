dfc86b597b4916d2dfb80904a8b5a71eac036aa7
Tarun <tk@gmail.com>
2021-09-12 09:28:35 +0530

    main file transferred to .gitignore

--- src/Main.java
+++ src/Main.java
@@ -68,52 +68,6 @@
 //
 //}
 
-class Tree {
-
-    static class Node {
-        int key;
-        Node left, right;
-        Node(int key){
-            this.key = key;
-            left = right = null;
-        }
-    }
-
-    Node root;
-
-    int height(Node node){
-        if (node == null) {
-            return 0;
-        }
-        return (1+Math.max(height(node.left), height(node.right)));
-    }
-
-    int diameter(Node node){
-        if (node == null)
-            return 0;
-        int left_height = height(node.left);
-        int right_height = height(node.right);
-        int left_diameter = diameter(node.left);
-        int right_diameter = diameter(node.right);
-        return Math.max(
-                left_height+right_height+1,
-                Math.max(left_diameter, right_diameter)
-        );
-    }
-
-    public static void main(String[] args) {
-        Tree tree = new Tree();
-        tree.root = new Node(1);
-        tree.root.left = new Node(2);
-        tree.root.right = new Node(3);
-        tree.root.left.left = new Node(4);
-        tree.root.left.right = new Node(5);
-        System.out.println(Math.abs(6-7));
-        System.out.println(Math.abs(8 -7));
-    }
-
-}
-
 class Main {
 
     static class Node {
@@ -131,78 +85,106 @@ class Main {
         int min, max, size, ans;
         boolean isBST;
         NodeInfo(){}
-        NodeInfo(int min, int max, int size, int ans, boolean isBST) {
+        public NodeInfo(int min, int max, int size, int ans, boolean isBST) {
             this.min = min;
             this.max = max;
             this.size = size;
             this.ans = ans;
             this.isBST = isBST;
         }
-        static int MIN = Integer.MIN_VALUE;
-        static int MAX = Integer.MAX_VALUE;
+    }
 
-        NodeInfo largestBST(Node node){
-            if (node == null)
-                return new NodeInfo(MIN, MAX, 0, 0, true);
-            if (node.left == null && node.right == null)
-                return new NodeInfo(node.key, node.key, 1, 1, true);
+    static int MIN = Integer.MIN_VALUE;
+    static int MAX = Integer.MAX_VALUE;
+    static NodeInfo largestBST(Node node){
+        if (node == null)
+            return new NodeInfo(MIN, MAX, 0, 0, true);
+        if (node.left == null && node.right == null)
+            return new NodeInfo(node.key, node.key, 1, 1, true);
 
-            NodeInfo leftNode = largestBST(node.left);
-            NodeInfo rightNode = largestBST(node.right);
+        NodeInfo leftNode = largestBST(node.left);
+        NodeInfo rightNode = largestBST(node.right);
 
-            NodeInfo returnInfo = new NodeInfo();
-            returnInfo.size = 1+ leftNode.size + rightNode.size;
+        NodeInfo returnNode = new NodeInfo();
+        returnNode.size = 1+ leftNode.size+ rightNode.size;
 
-            if (leftNode.isBST && rightNode.isBST && leftNode.max<node.key && rightNode.min > node.key){
-                returnInfo.min = Math.min(Math.min(leftNode.min, rightNode.min), node.key);
-                returnInfo.max = Math.max(Math.min(leftNode.max, rightNode.max), node.key);
-                returnInfo.ans = returnInfo.size;
-                returnInfo.isBST = true;
-                return returnInfo;
-            }
-
-            returnInfo.ans = Math.max(leftNode.ans, rightNode.ans);
-            returnInfo.isBST = false;
-            return returnInfo;
-
+        if (leftNode.max<node.key && rightNode.min>node.key && leftNode.isBST && rightNode.isBST){
+            returnNode.min = Math.min(Math.min(leftNode.min, rightNode.min), node.key);
+            returnNode.max = Math.max(Math.min(leftNode.max, rightNode.max), node.key);
+            returnNode.ans = returnNode.size;;
+            returnNode.isBST = true;
         }
 
+        returnNode.ans = Math.max(leftNode.ans, rightNode.ans);
+        returnNode.isBST = false;
+
+        return returnNode;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
-
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//class Main {
+//
+//    static class Node {
+//        int key;
+//        Node left, right;
+//        Node(int key){
+//            this.key = key;
+//            left = right = null;
+//        }
+//    }
+//
+//    Node root;
+//
+//    static class NodeInfo {
+//        int min, max, size, ans;
+//        boolean isBST;
+//        NodeInfo(){}
+//        NodeInfo(int min, int max, int size, int ans, boolean isBST) {
+//            this.min = min;
+//            this.max = max;
+//            this.size = size;
+//            this.ans = ans;
+//            this.isBST = isBST;
+//        }
+//        static int MIN = Integer.MIN_VALUE;
+//        static int MAX = Integer.MAX_VALUE;
+//
+//        NodeInfo largestBST(Node node){
+//            if (node == null)
+//                return new NodeInfo(MIN, MAX, 0, 0, true);
+//            if (node.left == null && node.right == null)
+//                return new NodeInfo(node.key, node.key, 1, 1, true);
+//
+//            NodeInfo leftNode = largestBST(node.left);
+//            NodeInfo rightNode = largestBST(node.right);
+//
+//            NodeInfo returnInfo = new NodeInfo();
+//            returnInfo.size = 1+ leftNode.size + rightNode.size;
+//
+//            if (leftNode.isBST && rightNode.isBST && leftNode.max<node.key && rightNode.min > node.key){
+//                returnInfo.min = Math.min(Math.min(leftNode.min, rightNode.min), node.key);
+//                returnInfo.max = Math.max(Math.min(leftNode.max, rightNode.max), node.key);
+//                returnInfo.ans = returnInfo.size;
+//                returnInfo.isBST = true;
+//                return returnInfo;
+//            }
+//
+//            returnInfo.ans = Math.max(leftNode.ans, rightNode.ans);
+//            returnInfo.isBST = false;
+//            return returnInfo;
+//
+//        }
+//
+//    }
+//
+//    public static void main(String[] args){
+//
+//    }
+//
+//}
